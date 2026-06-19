bea85a000acff7822cba38b494b8f76e1cc40538
Uplaksh Rajoria <raghavrajoria36@gmail.com>
2026-06-19 22:43:05 +0530

    [Uplaksh] committed the scenario based question on Java Input and Output

---
+++ gcr-codebase/control-flow/Java_I&O/ScenarioQuestion/DailyExpenseLogger.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class DailyExpenseLogger {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+
+            FileWriter fw =
+                    new FileWriter("expenses.txt", true);
+
+            System.out.print("Enter category: ");
+            String category = sc.nextLine();
+
+            System.out.print("Enter amount: ");
+            int amount = sc.nextInt();
+
+            fw.write(category + " - " + amount);
+            fw.write("\n");
+
+            fw.close();
+
+            System.out.println("Expense saved.");
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
